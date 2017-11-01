5649178489d89d85459dfc490fcc8a5469ef72b1
Sachin <sac9798@gmail.com>
2017-11-02 00:17:08 +0530

    Update sac.py

--- sac.py
+++ sac.py
@@ -2,6 +2,6 @@ for i in range(1,6):
     print(" ")
     for j in range(1,6-i):
         
-        for k in range(1,i+1):
+        for k in range(1,i+1+j):
             print(k,end=" ")
         print("\n")
