af370164168d9e4d1c724b910a64a503110238bd
rogers9798 <sac9798@gmail.com>
2017-11-02 21:54:46 +0530

    updatedsac

--- sac.py
+++ sac.py
@@ -1,7 +1,11 @@
 for i in range(1,6):
     print(" ")
     for j in range(1,6-i):
+<<<<<<< HEAD
 
+=======
+        
+>>>>>>> 5649178489d89d85459dfc490fcc8a5469ef72b1
         for k in range(1,i+1+j):
             print(k,end=" ")
         print("\n")
